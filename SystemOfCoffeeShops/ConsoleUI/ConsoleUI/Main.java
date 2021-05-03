3f9179035adc7c12ff092abaadea87747d2ecd12
Barış Aslan <baris.aslan100@gmail.com>
2021-05-04 02:49:57 +0300

    Upgrade Main

--- SystemOfCoffeeShops/ConsoleUI/ConsoleUI/Main.java
+++ SystemOfCoffeeShops/ConsoleUI/ConsoleUI/Main.java
@@ -7,14 +7,21 @@ import java.util.concurrent.CyclicBarrier;
 import Abstract.CustomerManager;
 import Adapters.MernisServiceAdapter;
 import Concrete.Customer;
+import Concrete.NeroCustomerManager;
 import Concrete.StarbucksCustomerManager;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		CustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+
+		CustomerManager customerManager2 = new NeroCustomerManager();
+
 		Customer customer = new Customer(1, "20110795948", "Barýþ", "Aslan", LocalDate.of(2000, 3, 2));
+
 		customerManager.add(customer);
+		customerManager2.add(customer);
 
 	}
 
